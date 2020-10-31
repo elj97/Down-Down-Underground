0f61f50823d3419bc176bd1e77b19543ae46ca45
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-10-31 19:13:16 +0800

    Player can move and mine
    as title says :)))

---
+++ Down Down Underground/Assets/Mine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+
+    public float miningTimeDur = 0.4f; //How many seconds it takes to mine a block
+    float miningTime = 0f; //The timer for mining blocks
+    bool mined = false;
+
+    private void Awake()
+    {
+        miningTime = miningTimeDur;
+        mined = false;
+    }
+
+    void Update()
+    {
+        if (miningTime > 0)
+        {
+            miningTime -= Time.deltaTime;
+            mined = false;
+        }
+        if (miningTime < 0)
+        {
+            mined = true;
+            miningTime = miningTimeDur;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (mined == true)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
