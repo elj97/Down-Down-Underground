f730ca43c22706bbb4031461aa420031b544767b
elj97 <61007820+elj97@users.noreply.github.com>
2020-11-06 00:11:39 +0800

    trying to fix laser

---
+++ Down Down Underground/Assets/Greybox/Scripts/LaserStream.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserStream : MonoBehaviour
+{
+    public Transform mParticleSystem;
+
+    private MeshRenderer mMeshRenderer;
+    
+    void Awake()
+    {
+        mMeshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        UpdateStream(GetHeight(other));
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        UpdateStream(0);
+    }
+
+    private float GetHeight(Collider collider)
+    {
+        return collider.transform.position.y + collider.bounds.size.y;
+    }
+
+    private void UpdateStream(float newHeight)
+    {
+        // Particle
+        Vector3 newPosition = new Vector3(transform.position.x, newHeight, transform.position.z);
+        mParticleSystem.position = newPosition;
+
+        // Height cutoff
+        newHeight /= transform.localScale.y;
+        mMeshRenderer.material.SetFloat("_Cuttoff", newHeight);
+    }
+}
