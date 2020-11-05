981fd2f63957bbc1726915514aa03148420ff0ea
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-11-05 20:28:59 +0800

    Changing Movement (still broken!)
    :(((

--- Down Down Underground/Assets/Greybox/Scripts/GameManager.cs
+++ Down Down Underground/Assets/Greybox/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 /*
-    Script: GameManager_Test1
+    Script: Based on GameManager_Test1
     Author: Gareth Lockett
     Version: 1.0
     Description:    Super simple example moving player cubes around using input from SAE arcade machine.
@@ -10,7 +10,7 @@
 
 namespace SAE
 {
-    public class GameManager_Test1 : MonoBehaviour
+    public class GameManager : MonoBehaviour
     {
         // Properties
         public Rigidbody yellowPlayer;          // Reference to yellow players' Rigidbody component.
@@ -25,25 +25,25 @@ namespace SAE
         private void Update()
         {
             // Poll SAE.ArcadeMachine for players' joystick axis and use to set Rigidbody velocity.
-            Vector2 axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic( ArcadeMachine.PlayerColorId.YELLOW_PLAYER );
-            this.yellowPlayer.velocity = new Vector3( axisValues.x, 0f, -axisValues.y ) * this.moveSpeed;
+            Vector2 axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic(ArcadeMachine.PlayerColorId.YELLOW_PLAYER);
+            this.yellowPlayer.velocity = new Vector3(axisValues.x, 0f, -axisValues.y) * this.moveSpeed;
 
-            axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic( ArcadeMachine.PlayerColorId.BLUE_PLAYER );
-            this.bluePlayer.velocity = new Vector3( axisValues.x, 0f, -axisValues.y ) * this.moveSpeed;
+            axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic(ArcadeMachine.PlayerColorId.BLUE_PLAYER);
+            this.bluePlayer.velocity = new Vector3(axisValues.x, 0f, -axisValues.y) * this.moveSpeed;
 
-            axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic( ArcadeMachine.PlayerColorId.RED_PLAYER );
-            this.redPlayer.velocity = new Vector3( axisValues.x, 0f, -axisValues.y ) * this.moveSpeed;
+            axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic(ArcadeMachine.PlayerColorId.RED_PLAYER);
+            this.redPlayer.velocity = new Vector3(axisValues.x, 0f, -axisValues.y) * this.moveSpeed;
 
-            axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic( ArcadeMachine.PlayerColorId.GREEN_PLAYER );
-            this.greenPlayer.velocity = new Vector3( axisValues.x, 0f, -axisValues.y ) * this.moveSpeed;
+            axisValues = SAE.ArcadeMachine.PlayerJoystickAxisStatic(ArcadeMachine.PlayerColorId.GREEN_PLAYER);
+            this.greenPlayer.velocity = new Vector3(axisValues.x, 0f, -axisValues.y) * this.moveSpeed;
 
             // Poll SAE.ArcadeMachine for YELLOW players' button 0 (True if held down)
-            if( SAE.ArcadeMachine.PlayerPressingButtonStatic( ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 0 ) == true )
-            { Debug.Log( "YELLOW player pressed button 0 (Held down)" ); }
+            if (SAE.ArcadeMachine.PlayerPressingButtonStatic(ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 0) == true)
+            { Debug.Log("YELLOW player pressed button 0 (Held down)"); }
 
             // Poll SAE.ArcadeMachine for YELLOW players' button 1 (True if pressed once .. eg. not held down)
-            if( SAE.ArcadeMachine.PlayerPressingButtonStatic( ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 1, true ) == true )
-            { Debug.Log( "YELLOW player pressed button 1 (Not held down)" ); }
+            if (SAE.ArcadeMachine.PlayerPressingButtonStatic(ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 1, true) == true)
+            { Debug.Log("YELLOW player pressed button 1 (Not held down)"); }
 
         }
     }
--- Down Down Underground/Assets/Greybox/Scripts/PlayerMovement.cs
+++ Down Down Underground/Assets/Greybox/Scripts/PlayerMovement.cs
@@ -2,143 +2,149 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace SAE
 {
-
-    public GameObject mineCircle;
-    public float mineCircleOffSet = 0.65f;
-    public float speed;
-    float horizontalMove = 0f;
-    float verticalMove = 0f;
-
-    bool currentlyMovingVert = false;
-    bool currentlyMovingHori = false;
-
-    float direction = 0f; //The direction the player is facing/currently-moving, is determined by number (0 = right, 1 = down, 2 = left, 3 = up)
-    
-    public enum miningDirection
+    public class PlayerMovement : MonoBehaviour
     {
-        NONE,UP,DOWN,LEFT,RIGHT
-    }
-    miningDirection mDirection;
 
-    private void Start()
-    {
-     mDirection = miningDirection.NONE;
-    }
-    void Update()
-    {
-        //Right and Left Input for Keyboard
-        if (Input.GetKey(KeyCode.D))
-        {
-            horizontalMove = speed; //Sets speed
-            currentlyMovingVert = true; //Indicates current movement
-            //direction = 0f; //Sets direction to the right
-            mDirection = miningDirection.RIGHT;
-        } else if (Input.GetKey(KeyCode.A))
-        {
-            horizontalMove = -speed; //Sets speed
-            currentlyMovingVert = true; //Indicates current movement
-            //direction = 2f; //Sets direction to the left
-            mDirection = miningDirection.LEFT;
-        } else
-        {
-            horizontalMove = 0f; //Sets speed
-            currentlyMovingVert = false; //Indicates current movement
-        }
-        //Up and Down Input for Keyboard
-        if (Input.GetKey(KeyCode.W))
-        {
-            verticalMove = speed; //Sets speed
-            currentlyMovingHori = true; //Indicates current movement
-            //direction = 3f; //Sets direction to up
-            mDirection = miningDirection.UP;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            verticalMove = -speed; //Sets speed
-            currentlyMovingHori = true; //Indicates current movement
-            //direction = 1f; //Sets direction to down
-            mDirection = miningDirection.DOWN;
-        }
-        else
-        {
-            verticalMove = 0f; //Sets speed
-            currentlyMovingHori = false; //Indicates current movement
-        }
+        public GameObject mineCircle;
+        public float mineCircleOffSet = 0.65f;
+        public float speed;
+        float horizontalMove = 0f;
+        float verticalMove = 0f;
 
-        //Activates or Deactivates the mining circle based on whether the player is moving or not
-        if (mineCircle.activeInHierarchy == true)
+        bool currentlyMovingVert = false;
+        bool currentlyMovingHori = false;
+
+        float direction = 0f; //The direction the player is facing/currently-moving, is determined by number (0 = right, 1 = down, 2 = left, 3 = up)
+
+        public enum miningDirection
         {
-            if (currentlyMovingHori == false && currentlyMovingVert == false)
+            NONE, UP, DOWN, LEFT, RIGHT
+        }
+        miningDirection mDirection;
+
+        private void Start()
+        {
+            mDirection = miningDirection.NONE;
+        }
+        void Update()
+        {
+            //Right and Left Input for Keyboard
+            if (Input.GetKey(KeyCode.D))
             {
-                mineCircle.SetActive(false);
+                horizontalMove = speed; //Sets speed
+                currentlyMovingVert = true; //Indicates current movement
+                                            //direction = 0f; //Sets direction to the right
+                mDirection = miningDirection.RIGHT;
             }
-        } else
-        {
-            if (currentlyMovingHori == true)
+            else if (Input.GetKey(KeyCode.A))
             {
-                mineCircle.SetActive(true);
+                horizontalMove = -speed; //Sets speed
+                currentlyMovingVert = true; //Indicates current movement
+                                            //direction = 2f; //Sets direction to the left
+                mDirection = miningDirection.LEFT;
             }
-            if (currentlyMovingVert == true)
+            else
             {
-                mineCircle.SetActive(true);
+                horizontalMove = 0f; //Sets speed
+                currentlyMovingVert = false; //Indicates current movement
             }
+            //Up and Down Input for Keyboard
+            if (Input.GetKey(KeyCode.W))
+            {
+                verticalMove = speed; //Sets speed
+                currentlyMovingHori = true; //Indicates current movement
+                                            //direction = 3f; //Sets direction to up
+                mDirection = miningDirection.UP;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                verticalMove = -speed; //Sets speed
+                currentlyMovingHori = true; //Indicates current movement
+                                            //direction = 1f; //Sets direction to down
+                mDirection = miningDirection.DOWN;
+            }
+            else
+            {
+                verticalMove = 0f; //Sets speed
+                currentlyMovingHori = false; //Indicates current movement
+            }
+
+            //Activates or Deactivates the mining circle based on whether the player is moving or not
+            if (mineCircle.activeInHierarchy == true)
+            {
+                if (currentlyMovingHori == false && currentlyMovingVert == false)
+                {
+                    mineCircle.SetActive(false);
+                }
+            }
+            else
+            {
+                if (currentlyMovingHori == true)
+                {
+                    mineCircle.SetActive(true);
+                }
+                if (currentlyMovingVert == true)
+                {
+                    mineCircle.SetActive(true);
+                }
+            }
+
+            //Moves minecircle depending on direction facing, depends on the minecircle off set
+            /*  if (direction == 0f)
+              {
+                  mineCircle.transform.position = transform.position + new Vector3(mineCircleOffSet, 0f, 0f);
+              } else if (direction == 1f)
+              {
+                  mineCircle.transform.position = transform.position + new Vector3(0f, -mineCircleOffSet, 0f);
+              } else if (direction == 2f)
+              {
+                  mineCircle.transform.position = transform.position + new Vector3(-mineCircleOffSet, 0f, 0f);
+              } else if (direction == 3f)
+              {
+                  mineCircle.transform.position = transform.position + new Vector3(0f, mineCircleOffSet, 0f);
+              }*/
+
+            switch (mDirection)
+            {
+                case miningDirection.NONE:
+                    break;
+                case miningDirection.UP:
+                    Debug.Log("UP");
+                    mineCircle.transform.position = transform.position + new Vector3(0f, mineCircleOffSet, 0f);
+                    break;
+                case miningDirection.DOWN:
+                    Debug.Log("DOWN");
+                    mineCircle.transform.position = transform.position + new Vector3(0f, -mineCircleOffSet, 0f);
+                    break;
+                case miningDirection.LEFT:
+                    Debug.Log("LEFT");
+                    mineCircle.transform.position = transform.position + new Vector3(-mineCircleOffSet, 0f, 0f); //left
+                    break;
+                case miningDirection.RIGHT:
+                    Debug.Log("RIGHT");
+                    mineCircle.transform.position = transform.position + new Vector3(mineCircleOffSet, 0f, 0f); //right
+                    break;
+            }
+
+            ///Debug Lines for the current movement
+            //Debug.Log("currentlyMovingHori = " + currentlyMovingHori);
+            //Debug.Log("currentlyMovingVert = " + currentlyMovingVert);
+
         }
 
-        //Moves minecircle depending on direction facing, depends on the minecircle off set
-      /*  if (direction == 0f)
+        private void FixedUpdate()
         {
-            mineCircle.transform.position = transform.position + new Vector3(mineCircleOffSet, 0f, 0f);
-        } else if (direction == 1f)
-        {
-            mineCircle.transform.position = transform.position + new Vector3(0f, -mineCircleOffSet, 0f);
-        } else if (direction == 2f)
-        {
-            mineCircle.transform.position = transform.position + new Vector3(-mineCircleOffSet, 0f, 0f);
-        } else if (direction == 3f)
-        {
-            mineCircle.transform.position = transform.position + new Vector3(0f, mineCircleOffSet, 0f);
-        }*/
-
-        switch (mDirection)
-        {
-            case miningDirection.NONE:
-                break;
-            case miningDirection.UP:
-                Debug.Log("UP");
-                mineCircle.transform.position = transform.position + new Vector3(0f, mineCircleOffSet, 0f);
-                break;
-            case miningDirection.DOWN:
-                Debug.Log("DOWN");
-                mineCircle.transform.position = transform.position + new Vector3(0f, -mineCircleOffSet, 0f);
-                break;
-            case miningDirection.LEFT:
-                Debug.Log("LEFT");
-                mineCircle.transform.position = transform.position + new Vector3(-mineCircleOffSet, 0f, 0f); //left
-                break;
-            case miningDirection.RIGHT:
-                Debug.Log("RIGHT");
-                mineCircle.transform.position = transform.position + new Vector3(mineCircleOffSet, 0f, 0f); //right
-                break;
-        }
-
-        ///Debug Lines for the current movement
-        //Debug.Log("currentlyMovingHori = " + currentlyMovingHori);
-        //Debug.Log("currentlyMovingVert = " + currentlyMovingVert);
-
-    }
-
-    private void FixedUpdate()
-    {
-        //Moves the player if the speed isn't at 0
-        if (horizontalMove != 0f)
-        {
-            transform.position += transform.TransformDirection(Vector3.right) * Time.fixedDeltaTime * horizontalMove;
-        }
-        if (verticalMove != 0f)
-        {
-            transform.position += transform.TransformDirection(Vector3.up) * Time.fixedDeltaTime * verticalMove;
+            //Moves the player if the speed isn't at 0
+            if (horizontalMove != 0f)
+            {
+                transform.position += transform.TransformDirection(Vector3.right) * Time.fixedDeltaTime * horizontalMove;
+            }
+            if (verticalMove != 0f)
+            {
+                transform.position += transform.TransformDirection(Vector3.up) * Time.fixedDeltaTime * verticalMove;
+            }
         }
     }
 }
