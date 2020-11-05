b06bf7f88ff0b99a09ceee924040f7324cb729c2
Gemma Ramage <1008468@student.sae.edu.au>
2020-11-05 19:24:26 +0800

    Main Menu stuff

---
+++ Down Down Underground/Assets/Menu/Menu UI/SceneChanger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Player 1");
+    }
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Player 2");
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Player 3");
+    }
+
+    public void Scene4()
+    {
+        SceneManager.LoadScene("Player 4");
+    }
+
+    public void Scene5()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void Scene6()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Scene7()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
