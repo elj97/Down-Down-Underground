166408378ff72740c6a7da03baf3a5b8553bd530
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-11-08 22:13:15 +0800

    Added A* and Tried to get enemies to work
    Title says all

---
+++ Down Down Underground/Assets/Enemies/Scripts/GenericEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericEnemy : MonoBehaviour
+{
+
+    public float health;
+    public float moveSpeed;
+
+    bool patrolling;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject, 0f);
+        }
+    }
+
+}
