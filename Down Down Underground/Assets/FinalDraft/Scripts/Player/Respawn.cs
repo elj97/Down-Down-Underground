8c948d9a8634383fd16a1cbee580693ac118a369
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-11-16 01:48:25 +0800

    Made a Draft Scene for Final Version
    As title + combined some Elijah branch and Enemies branch items

---
+++ Down Down Underground/Assets/FinalDraft/Scripts/Player/Respawn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject Player;
+    [SerializeField] private Transform Playermodel;
+    [SerializeField] private Transform respawnPoint;
+    public float spawnDelay = 2f;
+
+    /*  //script that works
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = respawnPoint.transform.position;
+    }*/
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player.SetActive(false);
+            StartCoroutine("Respawner", spawnDelay);
+        }
+    }
+
+    // here i want to get the player lives if last heart destroyed then die and start the respawner coroutine
+
+    IEnumerator Respawner(float Count)
+    {
+        yield return new WaitForSeconds(Count);
+        Playermodel.transform.position = respawnPoint.transform.position;
+        Player.SetActive(true);
+        yield return null;
+    }
+
+
+    // script to use coroutine
+    /*public GameObject player;
+    public Transform spawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            StartCoroutine("Respawner", 2f);
+        }
+    }
+
+    IEnumerator Respawner(float spawnDelay)
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(player, spawnPoint.position, spawnPoint.rotation);
+    }*/
+
+}
---
+++ Down Down Underground/Assets/FinalDraft/Scripts/Rock/Rock.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+
+    RockDetectGround rockDetectGround;
+    public float deathTime = 1f;
+    public float fallSpeed = 1f;
+    public float fallStartTime = 1f;
+    public ScanPath scanPath;
+
+    bool hasFallen = false;
+    bool destroyedCurrentGround = false;
+    bool scannedPath = false;
+
+    IEnumerator Fall(float Count)
+    {
+        yield return new WaitForSeconds(Count);
+        Fall();
+        yield return null;
+    }
+
+    void Start()
+    {
+        rockDetectGround = this.GetComponentInChildren<RockDetectGround>();
+    }
+    
+    void Update()
+    {
+        if (rockDetectGround.noGround == true)
+        {
+            StartCoroutine("Fall", fallStartTime);
+            hasFallen = true;
+        } else
+        {
+            if (hasFallen == true)
+            {
+                Destroy();
+                fallSpeed = 0f;
+            }
+        }
+    }
+
+    void Fall()
+    {
+        transform.position += transform.TransformDirection(Vector3.down) * Time.fixedDeltaTime * fallSpeed;
+    }
+
+    void Destroy()
+    {
+        Destroy(this.gameObject, deathTime);
+        if (scannedPath == false)
+        {
+            scanPath.InitiateCoroutine();
+            scannedPath = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (hasFallen == true && destroyedCurrentGround == false)
+            {
+                Destroy(other.gameObject, deathTime);
+                destroyedCurrentGround = true;
+            }
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Kill
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            //Kill
+        }
+    }
+}
