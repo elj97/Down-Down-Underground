d2f3fcf0661b90c3d4e3589e77dacfa4887dd2bb
elj97 <61007820+elj97@users.noreply.github.com>
2020-11-18 04:56:28 +0800

    try controller

--- Down Down Underground/Assets/Menu/MenuScripts/MenuController.cs
+++ Down Down Underground/Assets/Menu/MenuScripts/MenuController.cs
@@ -1,26 +1,54 @@
 ﻿using System.Collections;
-using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 namespace SAE
 {
     public class MenuController : MonoBehaviour
     {
-        public PlayerJoystickAxisEvent playerJoystickAxisEvents;
+        public bool level1Accessible;
+        public bool controlsAccessible;
+        public bool creditsAccessible;
+        public bool mainMenuAccessible;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
+  
         void Update()
         {
-
+            //GameplayLevel
+            if ( SAE.ArcadeMachine.input.PlayerPressingButton(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 1) == true )
+            {
+                if( level1Accessible == true ) 
+                { 
+                    SceneManager.LoadScene("Level1");  
+                }
+                
+            }
+            //Controls
+            if ( SAE.ArcadeMachine.input.PlayerPressingButton(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 2) == true )
+            {
+                if ( controlsAccessible == true )
+                {
+                    SceneManager.LoadScene("Controls");
+                }       
+            }
+            //Credits
+            if ( SAE.ArcadeMachine.input.PlayerPressingButton(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 3) == true )
+            {
+                if ( creditsAccessible == true )
+                {
+                    SceneManager.LoadScene("Credits");
+                }   
+            }
+            //Main Menu
+            if ( SAE.ArcadeMachine.input.PlayerPressingButton(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 4) == true )
+            {
+                if ( mainMenuAccessible == true )
+                {
+                    SceneManager.LoadScene("Main Menu");
+                }      
+            }
         }
     }
-
 }
