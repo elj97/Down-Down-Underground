b1447d2a09a97b61398c101dcaabdca7fe76b857
elj97 <61007820+elj97@users.noreply.github.com>
2020-11-18 03:08:54 +0800

    menu import

---
+++ Down Down Underground/Assets/Test/Menu/MenuScripts/SceneChanger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    /*public void Scene1()
+    {
+        SceneManager.LoadScene("Player 1");
+    }
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Player 2");
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Player 3");
+    }
+
+    public void Scene4()
+    {
+        SceneManager.LoadScene("Player 4");
+    }*/
+
+    public void Scene1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Scene4()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Scene5()
+    {
+        SceneManager.LoadScene("You Lose");
+    }
+
+    public void Scene6()
+    {
+        SceneManager.LoadScene("You Win");
+    }
+}
