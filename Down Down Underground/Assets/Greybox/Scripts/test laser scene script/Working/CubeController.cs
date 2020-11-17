85e0fb64fbd6f71a8e8ab7aec0f0035188aae54d
elj97 <61007820+elj97@users.noreply.github.com>
2020-11-17 16:11:06 +0800

    badlaser

--- Down Down Underground/Assets/Greybox/Scripts/test laser scene script/Working/CubeController.cs
+++ Down Down Underground/Assets/Greybox/Scripts/test laser scene script/Working/CubeController.cs
@@ -8,12 +8,18 @@ public class CubeController : MonoBehaviour
     public float speed = 5;
     private Vector2 movementInput;
 
+
     void Update()
     {
         transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
-
+   
     }
 
     public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
-    
+
+    public void OnShoot(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Shooting");
+    }
+
 }
