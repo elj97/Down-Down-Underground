0dfd1b35276887bec7bb680d8e9cfdf5af127baa
elj97 <61007820+elj97@users.noreply.github.com>
2020-11-18 03:59:17 +0800

    joystick cursed

---
+++ Down Down Underground/Assets/Menu/MenuScripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SAE
+{
+    public class MenuController : MonoBehaviour
+    {
+        public PlayerJoystickAxisEvent playerJoystickAxisEvents;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
